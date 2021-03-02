048d9b412c9bbac3bdb9ea2191b60fcf9e31cd04
pedrocarr <eng.pedro.amb@gmail.com>
2021-03-02 15:15:45 -0300

    controllers

--- app/controllers/project_participants_controller.rb
+++ app/controllers/project_participants_controller.rb
@@ -1,2 +1,7 @@
 class ProjectParticipantsController < ApplicationController
+  
+  def index
+    @project = Project.find(params[:id])
+    @participants = @project.participants
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :projects
+  resources :projects do
+    resources :projects_participants
+  end
   delete '/projects/:id', to: "projects#destroy", as: :projects_destroy
 end
