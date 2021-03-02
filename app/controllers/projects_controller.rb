99ce23dc95470cca74ca3a9e6643120ff6221e4f
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-02 18:42:29 -0300

    mexendo na instanciacão do primeiro participante

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,10 +15,19 @@ class ProjectsController < ApplicationController
     @project = Project.new(projects_params)
     @project.user = current_user
     if @project.save
-      ProjectParticipant.create(user_id: current_user.id, project_participant_id: 1, project_id: @project.id, is_founder: true, invited_on: DateTime.now, accepted_on: DateTime.now)
+      create_participant(@project)
       redirect_to project_path(@project)
     else
       render :new, notice: "Something went wrong. Try again "
+    end
+  end
+
+  def create_participant(project)
+    @project_participant = ProjectParticipant.new(user_id: current_user.id, project_participant_id: 1, project_id: project.id, is_founder: true, invited_on: DateTime.now, accepted_on: DateTime.now)
+    if @project_participant.save
+      return @project_participant, notice: "User is now a project participant"
+    else
+      render :new, notice: "something went wrong, user not saved as a project participant"
     end
   end
 
