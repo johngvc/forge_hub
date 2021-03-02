b8f2c7a504d275356ee1416fe692071e140b809e
pedrocarr <eng.pedro.amb@gmail.com>
2021-03-02 11:14:12 -0300

    projectscontrollers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,23 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+  def show
+    @project = Project.find(params[:user_id])
+  end
+  def new
+    
+  end
+  def create
+    
+  end
+  def edit
+    
+  end
+  def update
+    
+  end
+  def destroy
+    
+  end
 end
