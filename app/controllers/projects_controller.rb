a9526eb901b6e579e3ea945ae287cbe9c60ea480
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-03 12:49:51 -0300

    pequena correção do authenticate user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_id, only: [:edit, :update, :destroy]
 
   def index
