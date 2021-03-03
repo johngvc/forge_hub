f1b00fadf3d763ccade6539c8d692933ef409c63
Leonardo Oliveira <lcgoliveira@hotmail.com>
2021-03-03 17:15:54 -0300

    linkedinprofiledone

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :projects, through: :project_participants
+  devise :omniauthable, omniauth_providers: %i[linkedin]
 end
+
