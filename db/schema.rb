b542edd8f19f1e17ce6b9a77460324cacf6d7ff1
pedrocarr <eng.pedro.amb@gmail.com>
2021-03-03 11:34:30 -0300

    aaa

--- db/schema.rb
+++ db/schema.rb
@@ -52,12 +52,12 @@ ActiveRecord::Schema.define(version: 2021_03_03_014721) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
