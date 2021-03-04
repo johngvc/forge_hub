bc32dd6f24e3ac7bea5cf46679448899f9a1a106
joao gabriel <johngvc@gmail.com>
2021-03-03 23:43:51 -0300

    Começando a integrar designs feitos anteriormente

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 2021_03_03_161724) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
