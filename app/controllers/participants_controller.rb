9b724581be5bcda98f5197cdea1d6887531911e3
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-04 20:21:56 -0300

    index de participantes feito

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,9 +1,11 @@
 class ParticipantsController < ApplicationController
   # before_action :authenticate_user!, except: %i[index]
-  before_action :set_participant, only: %i[index new create edit]
+  before_action :set_participant, only: %i[new create edit]
 
   def index
+    @project = Project.find(params[:project_id])
     @participants = Participant.where(project_id: @project.id)
+    @current_participant = Participant.where(project_id: @project.id, user_id: current_user.id).first
   end
 
   def new
@@ -11,18 +13,17 @@ class ParticipantsController < ApplicationController
   end
 
   def create
-    @current_participant = Participant.where(project_id: params[:project_id], user_id: current_user.id).first
-    @participant = Participant.create(project_id: params[:project_id], user_id: params[:user_id], participant_id: @current_participant.id, invited_at: DateTime.now)
+    @invite_participant = Participant.where(project_id: params[:project_id], user_id: current_user.id).first
+    @participant = Participant.create(project_id: params[:project_id], user_id: params[:user_id], invite_participant_id: @invite_participant.id, invited_at: DateTime.now)
     if @participant.save
-      redirect_to project_path(params[:project_id]), notice: "#{@participant.user.name} is now a project participant"
+      redirect_to project_participants_path(params[:project_id]), notice: "#{@participant.user.name} is now a project participant"
     else
       render :new
     end
   end
 
   def edit
-    @invite = Participant.find(@participant.participant_id)
-    raise
+    @invite_participant = Participant.find(@participant.invite_participant_id)
   end
 
 
