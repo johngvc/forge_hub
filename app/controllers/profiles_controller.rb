fb5661ad471f25356d45151d4786dbe9da8a4ad2
Leonardo Oliveira <lcgoliveira@hotmail.com>
2021-03-04 17:18:34 -0300

    linkedin_profile_improvement

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,16 @@ class ProfilesController < ApplicationController
 
   def show
     @user = current_user
+
+    omniauth = request.env["omniauth.auth"]
+    first_name = omniauth[:info][:first_name]
+    last_name = omniauth[:info][:last_name]
+    # summary = omniauth[:extra][:raw_info][:summary]
+    # headline = omniauth[:extra][:raw_info][:headline]
+    image = omniauth[:info][:image]
+    email = omniauth[:info][:email]
+    # profile_url = omniauth[:info][:urls][:public_profile]
+
     @participations = Participant.where(user_id: @user.id)
     @projects = @participations.map do |participation|
       participation.project_id
