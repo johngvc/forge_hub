13326736c7393187196040e8219df71ff37bc0c5
pedrocarr <eng.pedro.amb@gmail.com>
2021-03-04 11:43:27 -0300

    pull do pedro

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_03_03_160347) do
+ActiveRecord::Schema.define(version: 2021_03_04_071851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,21 +60,6 @@ ActiveRecord::Schema.define(version: 2021_03_03_160347) do
     t.index ["user_id"], name: "index_participants_on_user_id"
   end
 
-  create_table "project_participants", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "project_participant_id", null: false
-    t.bigint "project_id", null: false
-    t.boolean "is_founder"
-    t.date "invited_on"
-    t.date "accepted_on"
-    t.string "clearence_level"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_id"], name: "index_project_participants_on_project_id"
-    t.index ["project_participant_id"], name: "index_project_participants_on_project_participant_id"
-    t.index ["user_id"], name: "index_project_participants_on_user_id"
-  end
-
   create_table "projects", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
@@ -91,17 +75,18 @@ ActiveRecord::Schema.define(version: 2021_03_03_160347) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.string "provider"
     t.string "uid"
     t.string "first_name"
     t.string "last_name"
     t.string "picture_url"
-    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -113,8 +98,5 @@ ActiveRecord::Schema.define(version: 2021_03_03_160347) do
   add_foreign_key "participants", "participants"
   add_foreign_key "participants", "projects"
   add_foreign_key "participants", "users"
-  add_foreign_key "project_participants", "project_participants"
-  add_foreign_key "project_participants", "projects"
-  add_foreign_key "project_participants", "users"
   add_foreign_key "projects", "users"
 end
