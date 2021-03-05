2e832ec96497da5f761662f2bb5cd01f8bd524a1
pedrocarr <eng.pedro.amb@gmail.com>
2021-03-04 22:37:20 -0300

    init modal done

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -1,6 +1,8 @@
+import 'jquery'
+
 const initModal = () => {
   $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   });
 }
- export { initModal };
+export { initModal };
