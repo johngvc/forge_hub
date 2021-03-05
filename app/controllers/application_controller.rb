07e25726972c2fe2a0c58abe0f999172ffab7e37
joao gabriel <johngvc@gmail.com>
2021-03-05 00:58:44 -0300

    Image hosting funcionando, concertei um problema que estava tendo com alguns parametros do devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :email, :password, :password_confirmation, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :email, :password, :password_confirmation, :current_password)}
   end
   include Pundit
 
