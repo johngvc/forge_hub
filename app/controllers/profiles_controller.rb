47516ab9883ca7a8e1188575e89618c7d1245b87
joao gabriel <johngvc@gmail.com>
2021-03-06 02:16:03 -0300

    Estilizados varias rotas de projeto, ajustado algumas coisas no user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   # before_action :set_adoption_requests, only: %i[show]
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     # authorize @user
 
     # omniauth = request.env["omniauth.auth"]
