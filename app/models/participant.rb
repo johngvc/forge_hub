e79e03503489b8b20040f601b99e14703a6560a8
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-07 18:31:16 -0300

    ajustei o index dos participantes para permitir edicão de status com simple_form, falta atualiza os métodos

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,6 +2,6 @@ class Participant < ApplicationRecord
   belongs_to :user
   belongs_to :project
   has_many :join_requests, dependent: :delete_all
-  validates :status, inclusion: { in: %w(invitee member founder),
+  validates :status, inclusion: { in: %w(invitee member founder cofounder),
     message: "%{value} is not a valid project status" }
 end
