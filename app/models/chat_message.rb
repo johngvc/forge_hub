d480897d11d7c01fb162f2b097c75a3051a212fd
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-09 13:26:40 -0300

    fiz as associacões com as demais classes.

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -1,2 +1,5 @@
 class ChatMessage < ApplicationRecord
+  belongs_to :user_sender_id, class_name: :User, foreign_key: true
+  belongs_to :user_receiver_id, class_name: :User, foreign_key: true
+  belongs_to :project
 end
