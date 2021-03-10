850f6ce306512ddd12a88fe4bd427f51b1001f63
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-10 16:13:07 -0300

    acrescentei os trackables de login, ultimo login e ip do devise à db de usuários. rollback na ultima migração

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
+
   def auth
     omniauth = request.env['omniauth.auth']
   end
-  
+
   def linkedin
-    user = User.where(uid: auth.uid).first_or_create! do |user| 
+    user = User.where(uid: auth.uid).first_or_create! do |user|
       user.uid = auth.uid
       user.provider = "linkedin"
       user.first_name = auth.info['first_name']
       user.last_name = auth.info['last_name']
       user.name = "#{auth.info['first_name']} #{auth.info['last_name']}"
       user.email = auth.info['email']
-      user.picture_url =auth.info['picture_url']
+      user.picture_url = auth.info['picture_url']
       user.password = Devise.friendly_token.first(8)
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_184923) do
+ActiveRecord::Schema.define(version: 2021_03_09_164839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,11 @@ ActiveRecord::Schema.define(version: 2021_03_10_184923) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
@@ -115,8 +120,6 @@ ActiveRecord::Schema.define(version: 2021_03_10_184923) do
     t.string "picture_url"
     t.boolean "admin"
     t.bigint "bootcamps_id"
-    t.datetime "last_login"
-    t.datetime "current_login"
     t.index ["bootcamps_id"], name: "index_users_on_bootcamps_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
