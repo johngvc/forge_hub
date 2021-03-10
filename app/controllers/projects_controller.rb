d34bea00b1d991495f3d2a612e9b0e681f5a006b
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-10 17:53:35 -0300

    atualização do projeto com suspensão consertado

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -137,6 +137,6 @@ class ProjectsController < ApplicationController
   end
 
   def projects_params
-    params.require(:project).permit(:id, :name, :description, :linkedin_url, :github_url, :trello_url, :photo)
+    params.require(:project).permit(:id, :name, :description, :linkedin_url, :github_url, :trello_url, :photo, :is_suspended)
   end
 end
