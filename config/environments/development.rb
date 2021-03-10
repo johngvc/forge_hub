b2560c02488409068e6debbcba5265dd9496954c
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-10 10:50:55 -0300

    alinhamentos de produção para o dia de hoje

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,4 @@
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-  # Bullet.growl         = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
-  end
 
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
