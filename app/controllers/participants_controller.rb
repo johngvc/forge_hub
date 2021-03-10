3ef1e479a858c4d6c1b83db839796d31d66f7b3f
Pedro Faria <pedrobentodefaria@gmail.com>
2021-03-10 18:43:00 -0300

    consertei a condicional do botão de sair do projeto, que só deixa o founder sair, se tiver mais de um founder

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -8,6 +8,8 @@ class ParticipantsController < ApplicationController
   def index
     @participants = Participant.where(project_id: @project.id)
     @current_participant = Participant.where(project_id: @project.id, user_id: current_user.id).first
+    num_founders = @participants.select { |participant| participant.status = 'founder' }
+    num_founders.count > 2 ? @more_than_one_founder = true : @more_than_one_founder = false
   end
 
   def new
