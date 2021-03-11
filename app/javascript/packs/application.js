90fdab68516c4d6b4f63f3a4458a0163f6a7ac55
joao gabriel <johngvc@gmail.com>
2021-03-11 20:17:49 -0300

    Dropdown da página de members estilizado

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 import { initModal } from '../components/modal'
 import { msgModal } from '../components/modal_messages'
-import { myFunction } from '../components/dropdown'
 import "bootstrap";
 import 'jquery';
 import { event } from 'jquery';
