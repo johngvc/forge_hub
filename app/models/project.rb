b2b5753b7758ab4a8c915ac4ef6dd1cc4e947f14
joao gabriel <johngvc@gmail.com>
2021-03-28 14:57:19 -0300

    Implementado o basico do search_by_tag por pg_search

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,9 @@
 class Project < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :participants, dependent: :delete_all
   has_many :join_requests, dependent: :delete_all
-  has_many :project_tags, dependent: :destroy
+  has_many :project_tags, dependent: :delete_all
   has_many :tags, through: :project_tags
   has_many :chat_messages
   has_one_attached :photo
@@ -12,4 +13,12 @@ class Project < ApplicationRecord
   validates :description, presence: true, length: { maximum: 135 }
   validates :status_project, inclusion: { in: ['idea', 'design', 'pre-production', 'development', 'growth'],
                                           message: "%{value} is not a valid project status" }
+
+  pg_search_scope :global_search,
+                  associated_against: {
+                    tags: %i[name]
+                  },
+                  using: {
+                    tsearch: { prefix: true, any_word: true }
+                  }
 end
