da3877efd5b0a08218c63d09269e265940c9bfd8
joao gabriel <johngvc@gmail.com>
2021-04-05 23:38:12 -0300

    Started implementing Chatroom creation (Bug found - messages from one channel getting sent to another)

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,33 @@
+class ChatroomsController < ApplicationController
+  before_action :pundit_policy_authorized?
+
+  def create
+    @chatroom = Chatroom.new({
+                               name: params[:chatRoomName]
+                             })
+    if @chatroom.save
+      params[:participants_ids].each do |participant_id|
+        ChatroomParticipant.create({
+                                     user_id: participant_id,
+                                     chatroom_id: @chatroom.id
+                                   })
+      end
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "shared/message", locals: { message: @message })
+      )
+    end
+  end
+
+  def pundit_policy_scoped?
+    true
+  end
+
+  def pundit_policy_authorized?
+    true
+  end
+
+  def message_params
+    params.require(:message).permit(:chatRoomName, :participants_ids)
+  end
+end
---
+++ app/javascript/components/new_chatroom.js
@@ -0,0 +1,23 @@
+import { fetchWithToken } from "../components/fetch_with_token";
+
+const newChatroom = (event) => {
+  // Get new message button
+  const newMessageButton = document.getElementById("new-message-btn");
+
+  newMessageButton.addEventListener("click", (event) => {
+    const data = {
+      messageContent: document.getElementById("new-message-input").value,
+      activeChatRoomId: document.getElementById("messages").dataset.chatroomId,
+    };
+    fetchWithToken("/message", {
+      method: "POST",
+      headers: {
+        Accept: "text/html",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  });
+};
+
+export { newChatroom };
